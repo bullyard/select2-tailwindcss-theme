9be64419b2f1e26e1101d67c19c7f24bb67b559b
Erim Icel <erimicel@gmail.com>
2025-01-09 22:04:13 +0000

    Move tailwind config to root folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,10 @@ module.exports = {
   // The selector strategy replaced the class strategy in Tailwind CSS v3.4.1.
   darkMode: 'selector',
   content: [
-    './**/*.{html}',
+    './**/*.html',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
